f2b46fc1cdfa70cb08a3d7d93b05413dbfb674b0
JaydenDev <willsjayden2@gmail.com>
2022-12-23 14:54:36 -0600

    Revert "NextJS Port (1)"
    This reverts commit ad02116fb2bbb74228eb943b841e68ec0baa4447.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navbar from "./components/nav/nav";
+import "./index.css";
+import fetchData from "./functions.js";
 const React = require("react");
 
 function App() {
   return (
     <div className="App">
-      <link rel="stylesheet" href="./index.css"/>
       <Navbar />
       <header className="App-header">
         <input
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import fetchData from "../../functions";
 
 class Navbar extends React.Component {
   render() {
